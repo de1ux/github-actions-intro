7541b75a7371ba1dd842ba5f0b2f89c385d4b45f
de1ux <nathan@de1ux.com>
2021-07-15 15:55:16 -0600

    Update to use querying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 const stateToMessage = {
-    null: <p>Pinging {process.env.REACT_APP_API_URL}...</p>,
-    true: <p>Ping success!</p>,
-    false: <p>Ping to {process.env.REACT_APP_API_URL} failed... check console log</p>
+    null: <p>Querying {process.env.REACT_APP_API_URL}...</p>,
+    true: <p>Query success!</p>,
+    false: <p>Query to {process.env.REACT_APP_API_URL} failed... check console log</p>
 }
 
 function App() {
