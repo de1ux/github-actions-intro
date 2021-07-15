a484b3c702f8acbf1c5b65d8722a5a0d3910a7e5
de1ux <nathan@de1ux.com>
2021-07-15 16:12:55 -0600

    Update jest test to query

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Ping/i);
+  const linkElement = screen.getByText(/Query/i);
   expect(linkElement).toBeInTheDocument();
 });
